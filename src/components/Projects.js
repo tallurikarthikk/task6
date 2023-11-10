b0be7bd10adbd09653b9f98f65ecf80d468a0b90
tallurikarthikk <karthikchowdary2k4@gmail.com>
2023-11-10 22:26:19 +0530

    first commit

---
+++ src/components/Projects.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <section id="projects">
+      <h2>Recent Projects</h2>
+      <p>
+        I had done many web development projects in recent times
+      </p>
+    </section>
+  );
+}
+
+export default Projects;
